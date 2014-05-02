e59b1467d8304547b371eae0c51b7f888c49124f
dev02 <dev02@dev02-VirtualBox>
2014-05-02 16:45:33 +0900

    first

---
+++ GuardOn/src/com/guardon/option/impl/OptionServiceImpl.java
@@ -0,0 +1,46 @@
+package com.guardon.option.impl;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import com.guardon.option.OptionDAO;
+import com.guardon.option.OptionService;
+import com.guardon.option.domain.Option;
+
+public class OptionServiceImpl implements OptionService{
+	
+	@Inject
+	@Named("optionDAO")
+	OptionDAO optionDAO;
+
+	@Override
+	public void updateOption(Option option) throws Exception {
+		optionDAO.updateOption(option);
+	}
+
+	@Override
+	public int getPwdLength() throws Exception {
+		return optionDAO.getPwdLength();
+	}
+
+	@Override
+	public int getAutoLogout() throws Exception {
+		return optionDAO.getAutoLogout();
+	}
+
+	@Override
+	public int getOtpTimeLimit() throws Exception {
+		return optionDAO.getOtpTimeLimit();
+	}
+
+	@Override
+	public int getLoginFailedCount() throws Exception {
+		return optionDAO.getLoginFailedCount();
+	}
+
+	@Override
+	public String getPwdComplexity() throws Exception {
+		return optionDAO.getPwdComplexity();
+	}
+
+}
